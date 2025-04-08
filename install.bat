8cd9d05ddf86bd85bcdc6ef73dfe5cf06f844ad9
ag7dev <admin@femscripts.de>
2025-04-08 11:04:17 +0200

    add check to install script so no oopsy woopies installas

--- install.bat
+++ install.bat
@@ -1,5 +1,12 @@
 @echo off
 chcp 65001 >nul
+echo Warning: This will erase all data files. Are you sure you want to proceed? (y/n)
+set /p confirm=
+if /i "%confirm%" neq "y" (
+    echo Installation cancelled.
+    pause
+    exit /b 1
+)
 echo [1/3] Installing required files...
 python main.py install
 if %errorlevel% neq 0 (
@@ -7,6 +14,8 @@ if %errorlevel% neq 0 (
     pause
     exit /b 1
 )
+
+
 
 echo [2/3] Creating startup script...
 echo [3/3] Cleaning up installation...
